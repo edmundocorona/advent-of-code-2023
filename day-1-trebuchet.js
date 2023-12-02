a594dae896955be20bf41b0c2566aeec2cc5ce32
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-12-02 00:13:19 -0600

    interpret spelled numbers

--- day-1-trebuchet.js
+++ day-1-trebuchet.js
@@ -1,11 +1,58 @@
 const input = document.querySelector('pre').textContent;
 const array = input.split('\n');
+const spelledNumbers = {
+  'zero': 0,
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9,
+  'ten': 10,
+  'eleven': 11,
+  'twelve': 12,
+  'thirteen': 13,
+  'fourteen': 14,
+  'fifteen': 15,
+  'sixteen': 16,
+  'seventeen': 17,
+  'eighteen': 18,
+  'nineteen': 19,
+  'twenty': 20,
+  'thirty': 30,
+  'forty': 40,
+  'fifty': 50,
+  'sixty': 60,
+  'seventy': 70,
+  'eighty': 80,
+  'ninety': 90,
+}
 const keyValue = array.reduce((acc, cur) => {
   let firstDigit = 0;
   let secondDigit = 0;
+  let firstDigitAux;
+  let secondDigitAux;
   for (let i = 0; i < cur.length; i++) {
     if (Number.isInteger(+cur[i])) {
       firstDigit = cur[i];
+      break;
+    }
+
+    //replaceAll dynamic
+    for (let j = 3; j < 9; j++) {
+      if (i + j > cur.length) {
+        break;
+      }
+      if (spelledNumbers[cur.slice(i, i + j)] !== undefined) {
+        firstDigitAux = spelledNumbers[cur.slice(i, i + j)];
+        break;
+      }
+    }
+    if (firstDigitAux !== undefined) {
+      firstDigit = firstDigitAux;
       break;
     }
   }
@@ -14,7 +61,23 @@ const keyValue = array.reduce((acc, cur) => {
       secondDigit = cur[i];
       break;
     }
+
+    //replaceAll dynamic
+    for (let j = 2; j < 9; j++) {
+      if (i - j < 0) {
+        break;
+      }
+      if (spelledNumbers[cur.slice(i - j, i + 1)] !== undefined) {
+        secondDigitAux = spelledNumbers[cur.slice(i - j, i + 1)];
+        break;
+      }
+    }
+    if (secondDigitAux !== undefined) {
+      secondDigit = secondDigitAux;
+      break;
+    }
   }
-  console.log(`${firstDigit}${secondDigit}`);
+  // console.log(`${cur} ${firstDigit}${secondDigit}`);
   return acc + +`${firstDigit}${secondDigit}`;
 }, 0);
+console.log(keyValue);
