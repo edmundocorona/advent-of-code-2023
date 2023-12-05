60bf6da8c5b5fdc5751b7262c1a5e9b568f481d6
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-12-04 18:41:21 -0600

    calc sum of gears ratio

--- day-3-geat-ratios.js
+++ day-3-geat-ratios.js
@@ -1,98 +1,69 @@
 const content = document.querySelector('pre').textContent;
-const array = content.split('\n').slice(0, -1);
-// console.table(array);
+const array = content.split('\n').slice(0, 10);
 
-// const sumPartNumbers = array.reduce((accumulator, current) => {
-//   for (let i = 0; i < current.length; i++) {
-//     if (current[i].match(/[^.\d\n]/)) {
-//       console.log(current[i]);
-//     }
-//   }
-//   return accumulator + current;
-// }, 0);
+// sum all products
+let accumulator = 0;
+const sumProducts = function (product) {
+  accumulator += product;
+};
 
-const midState = new Array(array.length);
-for (let i = 0; i < midState.length; i++) {
-  midState[i] = new Array(array[i].length).fill('.');
-}
+// multiply the numbers
+const multiplyNumbers = function (array) {
+  return array[0] * array[1] + array[2] * array[3];
+};
 
-let sumPartNumbers = 0;
+// complete the number
+const lookLeftDigits = function (i, j) {
+  const leftCell = array[i][j - 1];
+  // if (leftCell === undefined) {
+  //   return '';
+  // }
+  if (leftCell !== undefined & leftCell.match(/\d/)) {
+    return lookLeftDigits(i, j - 1) + leftCell;
+  }
+  return '';
+};
 
+const lookRightDigits = function (i, j) {
+  const rightCell = array[i][j + 1];
+  if (rightCell !== undefined & rightCell.match(/\d/)) {
+    return rightCell + lookRightDigits(i, j + 1);
+  }
+  return '';
+};
+
+// look around for digits
+const getNumbersAround = function (i, j) {
+  const numbersAround = [];
+  for (let k = -1; k <= 1; k++) {
+    for (let l = -1; l <= 1; l++) {
+      if (0 <= i + k & i + k < array.length 
+        & 0 <= j + l & j + l < array[i].length) {
+        const cell = array[i + k][j + l];
+        if (cell !== undefined) {
+          if (cell.match(/\d/)) {
+            const leftDigits = lookLeftDigits(i + k, j + l);
+            const rightDigits = lookRightDigits(i + k, j + l);
+            const fullNumber = leftDigits + cell + rightDigits;
+            numbersAround.push(fullNumber);
+  
+            // jumps
+            l += rightDigits.length;
+          }
+        }
+      }
+    }
+  }
+  return numbersAround;
+};
+
+// find the asterisk
 for (let i = 0; i < array.length; i++) {
-  const current = array[i];
-  // const currentMidState = new Array(current.length).fill('.');
-  for (let j = 0; j < current.length; j++) {
-    if (current[j].match(/[^.\d\n]/)) {
-      if (i - 1 >= 0) {
-        if (j - 1 >= 0) {
-          midState[i - 1][j - 1] = array[i - 1][j - 1];          
-        }
-        midState[i - 1][j] = array[i - 1][j];
-        if (j + 1 < array[i - 1].length) {
-          midState[i - 1][j + 1] = array[i - 1][j + 1];
-        }
-      }
-
-      if (j - 1 >= 0) {
-        midState[i][j - 1] = array[i][j - 1];
-      }
-      midState[i][j] = array[i][j];
-      if (j + 1 < array[i].length) {
-        midState[i][j + 1] = array[i][j + 1];
-      }
-
-      if (i + 1 < array.length) {
-        if (j - 1 >= 0) {
-          midState[i + 1][j - 1] = array[i + 1][j - 1];          
-        }
-        midState[i + 1][j] = array[i + 1][j];
-        if (j + 1 < array[i + 1].length) {
-          midState[i + 1][j + 1] = array[i + 1][j + 1];
-        }
-      }
-    }
-  }
-  // midState.push(currentMidState);
-}
-
-console.log(midState);
-
-const searchLeft = function (i, j) {
-  if (j - 1 >= 0) {
-    if (array[i][j - 1].match(/\d/)) {
-      midState[i][j - 1] = array[i][j - 1];
-      searchLeft(i, j - 1);
+  for (let j = 0; j < array[i].length; j++) {
+    if (array[i][j].match(/\*/)) {
+      sumProducts(multiplyNumbers(getNumbersAround(i, j)));
     }
   }
 }
 
-const searchRight = function (i, j) {
-  if (j + 1 < array[i].length) {
-    if (array[i][j + 1].match(/\d/)) {
-      midState[i][j + 1] = array[i][j + 1];
-      searchRight(i, j + 1);
-    }
-  }
-}
-
-for (let i = 0; i < midState.length; i++) {
-  for (let j = 0; j < midState[i].length; j++) {
-    if (midState[i][j].match(/\d/)) {
-      searchLeft(i, j);
-      searchRight(i, j);
-    }
-  }
-}
-
-console.log(midState);
-
-for (i = 0; i < midState.length; i++) {
-  const numbers = midState[i].join('').match(/(\d+)/g);
-  console.log(numbers);
-  for (j = 0; j < numbers.length; j++) {
-
-    sumPartNumbers += +numbers[j];
-  }
-}
-
-console.log(sumPartNumbers);
+console.log(accumulator);
